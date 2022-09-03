340fe50cb27c783d76971625156570d300da9eea
lekipising <leeplanke@gmail.com>
2022-09-03 23:56:53 +0300

    fix: responsive success mobile

--- components/contact/success.tsx
+++ components/contact/success.tsx
@@ -6,7 +6,7 @@ export default function SuccessMessage({
   toggleNewForm: () => void;
 }) {
   return (
-    <div className="flex max-w-[20vw] flex-col items-center justify-center gap-6 text-center">
+    <div className="flex md:max-w-[20vw] w-full md:mt-0 mt-8 flex-col items-center justify-center gap-6 text-center">
       <h1 className="text-[26px] font-medium text-white">Thank you! 🤘</h1>
       <p className="text-[18px] font-medium text-gray-100">
         I have received your message! I will get back to you as soon.
