e656001ad7f7e4b99490da5c1242410e77868678
Liplan Lekipising <leeplanke@gmail.com>
2022-09-13 19:19:29 +0300

    chore: add reveal animation to project image

--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { projects } from "./projects";
 
 export interface Technology {
@@ -17,6 +17,9 @@ export interface Project {
 
 import { motion } from "framer-motion";
 
+const hiddenMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 30px, rgba(0,0,0,1) 30px, rgba(0,0,0,1) 30px)`;
+const visibleMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 0px, rgba(0,0,0,1) 0px, rgba(0,0,0,1) 30px)`;
+
 export default function Projects() {
   return (
     <section className="m-auto flex max-w-[95%] flex-col justify-center gap-5 py-8 md:relative md:top-1/2 md:-translate-y-2/3 md:flex-row md:py-0 ">
@@ -28,6 +31,7 @@ export default function Projects() {
 }
 
 function OneProject({ project, index }: { project: Project; index: number }) {
+  const [imageLoaded, setImageLoaded] = useState(false);
   return (
     <motion.div
       initial={{ y: 50 }}
@@ -42,7 +46,17 @@ function OneProject({ project, index }: { project: Project; index: number }) {
         {project.title}
       </h1>
       <div className="project relative h-[58vh] max-h-[470px] rounded-[10px] border-[1px] border-gray-200 bg-dark-300 pt-0 pb-32 transition-all duration-300 ease-in">
-        <div className="relative m-auto h-[20vh] max-h-[215px] w-full rounded-t-[15px] transition-all duration-150 ease-in group-hover:rounded-none">
+        <motion.div
+          initial={false}
+          animate={
+            imageLoaded
+              ? { WebkitMaskImage: visibleMask, maskImage: visibleMask }
+              : { WebkitMaskImage: hiddenMask, maskImage: hiddenMask }
+          }
+          transition={{ duration: 1, delay: 0.3 }}
+          viewport={{ once: true }}
+          className="relative m-auto h-[20vh] max-h-[215px] w-full rounded-t-[15px] transition-all duration-150 ease-in group-hover:rounded-none"
+        >
           <Image
             src={project.image}
             objectFit="cover"
@@ -50,8 +64,9 @@ function OneProject({ project, index }: { project: Project; index: number }) {
             className="rounded-t-[10px]"
             layout="fill"
             alt={project.title}
+            onLoadingComplete={() => setImageLoaded(true)}
           />
-        </div>
+        </motion.div>
         <div className="p-8 px-4">
           <p className="mb-5 text-[15px]">{project.description}</p>
         </div>
