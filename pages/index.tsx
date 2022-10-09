a9958fe54e12eed710992b7d78583773867f5865
Liplan Lekipising <leeplanke@gmail.com>
2022-10-09 10:45:50 +0300

    feat: add circles animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,20 +32,33 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="no-scrollbar m-auto mt-[5vh] h-[90vh] w-[90vw] overflow-y-scroll rounded-[8px] border-[1px] border-gray-200 bg-dark-200 transition-all duration-300 ease-in">
-      <div
-        id="spotlight"
-        className="bg-blur-two fixed left-0 top-0 z-40 h-[50px] w-[50px] rounded-full"
-      />
-      <div className="fixed inset-x-0 z-[100] hidden px-[5vw] md:block">
-        <NavBar activeLink={"_hello"} />
-      </div>
-      <FirstScreen />
-      <AboutMe />
-      <Projects />
-      <ContactMe />
-      <BottomBar />
-      {/* <MainLayout currentPage="_hello"></MainLayout> */}
-    </main>
+    <>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <main className="no-scrollbar m-auto mt-[5vh] h-[90vh] w-[90vw] overflow-y-scroll rounded-[8px] border-[1px] border-gray-200 bg-dark-200 transition-all duration-300 ease-in relative z-20">
+        <div
+          id="spotlight"
+          className="bg-blur-two fixed left-0 top-0 z-40 h-[50px] w-[50px] rounded-full"
+        />
+        <div className="fixed inset-x-0 z-[100] hidden px-[5vw] md:block">
+          <NavBar activeLink={"_hello"} />
+        </div>
+        <FirstScreen />
+        <AboutMe />
+        <Projects />
+        <ContactMe />
+        <BottomBar />
+        {/* <MainLayout currentPage="_hello"></MainLayout> */}
+      </main>
+    </>
   );
 }
