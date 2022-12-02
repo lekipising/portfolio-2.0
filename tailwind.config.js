27143b6ffd043c97a48170405cb8a8fbf130197a
Liplan Lekipising <leeplanke@gmail.com>
2022-12-02 10:08:59 +0200

    chore: update gray color to improve contrast

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
       colors: {
         white: "#E5E9F0",
         purple: { light: "#4D5BCE", heavy: "#5565E8" },
-        gray: { 100: "#607B96", 200: "#1E2D3D" },
+        gray: { 100: "#9aacbe", 200: "#1E2D3D" },
         yellow: "#FEA55F",
         dark: { 100: "#1C2B3A", 200: "#011627", 300: "#011221" },
         green: "#43d9ad",
