0b9eb068e1c05df19dc4a01a0f9e99629f9db2b3
Liplan Lekipising <leeplanke@gmail.com>
2023-01-11 17:21:42 +0200

    chore: update about me

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -6,24 +6,26 @@ export default function AboutMe() {
   return (
     <motion.section
       id="_about-me"
-      className="cursor-default bg-dark-100/20 p-6 text-[13px] font-medium leading-[150%] md:mb-32 text-gray-100 shadow-lg transition-all duration-300 ease-in hover:bg-dark-100/40 md:relative  md:m-auto md:w-max  md:rounded-[30px] md:p-16 md:text-[16px]"
+      className="cursor-default bg-dark-100/20 p-6 text-[13px] font-medium leading-[150%] text-gray-100 shadow-lg transition-all duration-300 ease-in hover:bg-dark-100/40 md:relative md:m-auto  md:mb-32 md:w-max  md:rounded-[30px] md:p-16 md:text-[16px]"
     >
       <h2 className="text-yellow transition-all duration-300 ease-in">
         About me
       </h2>
       <br />
 
-      <p className="transi-+tion-all duration-300 ease-in hover:text-white/70">
-        I am a software engineer with 3+ years of experience in web
-        development. <br />I enjoy building everything from small business
-        sites to rich interactive web apps.
+      <p className="transition-all duration-300 ease-in hover:text-white/70">
+        As an experienced and creative frontend developer, I bring websites to <br />
+        life with my expertise in the latest web technologies and a keen eye for <br />
+        design. I can unlock the full potential of your digital presence, creating an <br />
+        outstanding user experience that will take your business to the next
+        level.
       </p>
       <br />
       <p></p>
       <p className="transition-all duration-300 ease-in hover:text-white/70">
         If you are a business looking for a strong web product, recruiter
-        looking to <br />hire or a developer looking for mentorship, feel free
-        to contact me.
+        looking to <br />
+        hire or a developer looking for mentorship, feel free to contact me.
       </p>
       <br />
       <p className="transition-all duration-300 ease-in hover:text-white/70">
@@ -44,6 +46,9 @@ export default function AboutMe() {
         </p>
         <p className="asterisk text-yellow transition-all duration-300 ease-in hover:text-white/70">
           Styled components
+        </p>
+        <p className="asterisk text-yellow transition-all duration-300 ease-in hover:text-white/70">
+          GraphQL
         </p>
       </div>
     </motion.section>
