8acdc4cf6a8259cfc953bb263d4d17e92cace355
Liplan Lekipising <leeplanke@gmail.com>
2023-01-21 23:55:44 +0200

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin={"true"}
+            crossOrigin={"anonymous"}
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap"
