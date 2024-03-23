0b1376cc18f92d28255de712e868ed8676aba8a0
Liplan Lekipising <leeplanke@gmail.com>
2024-03-23 12:04:52 +0300

    update projects section

--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -14,6 +14,7 @@ export interface Project {
   link: string;
   image: string;
   status: "Active" | "On Hold" | "Completed";
+  features: string[];
 }
 
 import { motion } from "framer-motion";
@@ -22,8 +23,11 @@ import useIntersect from "../../utils/useIntersectionObserver";
 const hiddenMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 30px, rgba(0,0,0,1) 30px, rgba(0,0,0,1) 30px)`;
 const visibleMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 0px, rgba(0,0,0,1) 0px, rgba(0,0,0,1) 30px)`;
 
-export default function Projects({setIsVisible} : {setIsVisible: () => void}) {
-
+export default function Projects({
+  setIsVisible,
+}: {
+  setIsVisible: () => void;
+}) {
   // Call the useIntersect hook and receive the setNode and entry variables
   const { entry, setNode } = useIntersect({
     root: null, // The element used as the viewport for checking visibility, null means the browser viewport
@@ -49,9 +53,10 @@ export default function Projects({setIsVisible} : {setIsVisible: () => void}) {
       ref={observeRef}
       className="relative m-auto mb-32 mt-20 flex max-w-[95%] flex-col justify-center gap-12 py-8 pb-[10rem] md:relative md:mt-0 md:flex-row md:gap-5  md:py-32 md:pb-[0rem]"
     >
-      <div className="absolute -top-8 left-1/2 w-full -translate-x-1/2 text-center md:top-12 md:w-[520px] md:text-left">
+      <div className="absolute -top-12 left-1/2 w-full -translate-x-1/2 text-center md:top-12 md:w-[520px] md:text-left">
         <motion.h2 className="heading-gradient mx-auto text-lg font-semibold text-white">
-          Adventures in Development: <br className="block md:hidden" /> Top Personal Projects
+          Adventures in Development: <br className="block md:hidden" /> Top
+          Personal Projects
         </motion.h2>
         <motion.div
           initial={{ width: 0 }}
@@ -61,28 +66,17 @@ export default function Projects({setIsVisible} : {setIsVisible: () => void}) {
         />
       </div>
 
-      {projects.map((project, i) => (
-        <OneProject key={i} index={i} project={project} />
-      ))}
+      <div className="grid w-full grid-cols-1 gap-y-8">
+        {projects.map((project, i) => (
+          <OneProject key={i} index={i} project={project} />
+        ))}
+      </div>
     </section>
   );
 }
 
 function OneProject({ project, index }: { project: Project; index: number }) {
   const [imageLoaded, setImageLoaded] = useState(false);
-
-  const getStatusColors = () => {
-    switch (project.status) {
-      case "Active":
-        return {bgColor: "bg-green/30", iconColor: "bg-green", textColor: "text-green"};
-      case "On Hold":
-        return {bgColor: "bg-yellow/30", iconColor: "bg-yellow", textColor: "text-yellow"};
-      case "Completed":
-        return {bgColor: "bg-white/30", iconColor: "bg-white", textColor: "text-white"};
-      default:
-        break;
-    }
-  }
 
   return (
     <motion.div
@@ -93,13 +87,55 @@ function OneProject({ project, index }: { project: Project; index: number }) {
       viewport={{ amount: 0.5 }}
       whileHover={{ y: -10, transition: { duration: 0.2 } }}
       transition={{ duration: 0.5, ease: "easeIn", delay: 0.1 * index }}
-      className="group w-full md:w-[350px]"
+      className="group w-full"
       id={`project-${project.title.toLowerCase().replace(" ", "-")}`}
     >
-      <h3 className="mb-3 text-[16px] font-bold text-gray-100 group-hover:text-yellow/70">
-        {project.title}
-      </h3>
-      <div className="project relative h-[58vh] max-h-[470px] rounded-[10px] border-[1px] border-gray-200 bg-dark-300 pb-32 pt-0 transition-all duration-300 ease-in">
+      <div className="project relative flex h-[800px] flex-col justify-between gap-8 rounded-[10px] border-[1px] border-gray-200 bg-dark-300 p-6 transition-all duration-300 ease-in lg:h-[500px] lg:flex-row lg:gap-0">
+        <div className="flex w-full flex-col gap-6 lg:w-[45%]">
+          <h3 className="text-[20px] font-bold text-gray-300">
+            {project.title}
+          </h3>
+
+          <div className="px-0">
+            <p className="text-[14px] font-medium tracking-tight text-gray-100 xl:text-[14px]">
+              {project.description}
+            </p>
+          </div>
+
+          <div className="flex w-full flex-col text-gray-100">
+            <h4 className="mb-2 text-[16px] font-semibold">Features</h4>
+            <div className="flex flex-col gap-1">
+              {project.features.map((feature, i) => (
+                <p
+                  key={i}
+                  className="asterisk pl-4 text-[14px] font-medium tracking-tight xl:text-[14px]"
+                >
+                  {feature}
+                </p>
+              ))}
+            </div>
+          </div>
+
+          <div className="mt-auto flex flex-col gap-6">
+            <div className="flex gap-3">
+              {project.technologies.map((tech, i) => (
+                <OneTechnology key={i} tech={tech} />
+              ))}
+            </div>
+
+            <div className="flex w-full items-center justify-between">
+              <a
+                href={project.link}
+                target="_blank"
+                className="w-full rounded-[8px] border bg-dark-100 px-4 py-2 text-center text-sm transition-all duration-300 ease-in hover:border hover:border-yellow lg:w-max"
+                rel="noreferrer"
+              >
+                View project
+              </a>
+            </div>
+          </div>
+        </div>
+
         <motion.div
           initial={false}
           animate={
@@ -109,11 +145,11 @@ function OneProject({ project, index }: { project: Project; index: number }) {
           }
           transition={{ duration: 1, delay: 0.5 }}
           viewport={{ once: true, amount: 0.8 }}
-          className="relative m-auto h-[20vh] max-h-[215px] w-full rounded-t-[10px] transition-all duration-150 ease-in group-hover:rounded-none"
+          className="relative my-auto h-[calc(100%_-_8px)] w-full rounded-[10px] transition-all duration-150 ease-in group-hover:rounded-none lg:w-1/2"
         >
           <Image
             src={project.image}
-            className="rounded-t-[10px]"
+            className="rounded-[10px]"
             alt={project.title}
             onLoad={() => setImageLoaded(true)}
             fill
@@ -124,37 +160,6 @@ function OneProject({ project, index }: { project: Project; index: number }) {
             }}
           />
         </motion.div>
-        <div className="p-8 px-4 pt-4">
-          <p className="mb-5 text-[14px] font-medium tracking-tight xl:text-[14px]">
-            {project.description}
-          </p>
-        </div>
-
-
-
-        <div className="absolute bottom-4 left-4 right-4 flex flex-col gap-4">
-
-          <div className="w-full flex justify-between items-center"><a
-            href={project.link}
-            target="_blank"
-            className="w-max rounded-[8px] border bg-dark-100 px-4 py-2 text-sm transition-all duration-300 ease-in hover:border hover:border-yellow"
-            rel="noreferrer"
-          >
-            View project
-          </a>
-          <div className={`${getStatusColors().bgColor} w-max h-max rounded-full flex items-center px-2 py-1 gap-3`}>
-          <div className={`h-3 w-3 rounded-full ${getStatusColors().iconColor}`} />
-          <span className={`${getStatusColors().textColor} text-xs`}>{project.status}</span>
-        </div>
-</div>
-
-
-          <div className="flex gap-3">
-            {project.technologies.map((tech, i) => (
-              <OneTechnology key={i} tech={tech} />
-            ))}
-          </div>
-        </div>
       </div>
     </motion.div>
   );
