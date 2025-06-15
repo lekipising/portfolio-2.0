f4b4e25fc6ad9ff26acc758ec8bbe28369acda1d
lekipising <leeplanke@gmail.com>
2025-06-15 16:12:25 +0300

    hero and resume updates

--- components/landing/index.tsx
+++ components/landing/index.tsx
@@ -1,50 +1,80 @@
 import Image from "next/image";
 import React, { useEffect, useRef } from "react";
 import useIntersect from "../../utils/useIntersectionObserver";
+import { scrollIntoId } from "../common/navbar";
 
-export default function FirstScreen({setIsVisible} : {setIsVisible: () => void}) {
-   // Call the useIntersect hook and receive the setNode and entry variables
-  const {entry, setNode} = useIntersect({
+export default function FirstScreen({
+  setIsVisible,
+}: {
+  setIsVisible: () => void;
+}) {
+  // Call the useIntersect hook and receive the setNode and entry variables
+  const { entry, setNode } = useIntersect({
     root: null, // The element used as the viewport for checking visibility, null means the browser viewport
-    rootMargin: '0px', // Margin around the root element (in pixels)
+    rootMargin: "0px", // Margin around the root element (in pixels)
     threshold: 0.5, // A threshold value between 0 and 1, indicating the percentage of the target element that should be visible before the callback is invoked
   });
 
-   const observeRef = useRef(null);
+  const observeRef = useRef(null);
 
   useEffect(() => {
-setNode(observeRef.current)
-  }, [])
+    setNode(observeRef.current);
+  }, []);
 
-useEffect(() => {
+  useEffect(() => {
     if (!!entry?.isIntersecting) {
       setIsVisible();
     }
   }, [entry?.isIntersecting]);
 
-
   return (
     <section
       id="_hello"
       ref={observeRef}
-      className="relative flex h-auto flex-col items-center justify-start gap-32 pb-16 pt-16 md:h-full md:flex-row md:justify-center md:pb-0 md:pt-0 "
+      className="relative flex h-auto flex-col items-center justify-start gap-32 pb-16 pt-16 md:h-full md:flex-row md:justify-center md:pb-0 md:pt-0"
     >
-      <div className="flex w-[90%] flex-col gap-16 md:w-auto">
+      <div className="flex w-[90%] flex-col gap-6 md:w-auto">
         <div>
           <p className="text-[18px] font-medium text-white">Hi. I am</p>
           <h1
             id="title"
-            className="title md:text-[62px] text-[48px] tracking-tighter text-white"
+            className="title text-[48px] tracking-tighter text-white md:text-[62px]"
           >
             Liplan Lekipising
           </h1>
-          <p className="md:text-[32px] text-[23px] font-medium tracking-tight text-green">
-            &gt; Software Engineer
+          <p className="text-[23px] font-medium tracking-tight text-green md:text-[32px]">
+            &gt; Senior Software Engineer
           </p>
         </div>
-        <div className="flex flex-col gap-2 italic text-gray-100 md:gap-1">
-          <p>Are you ready to see some web wizardry?</p>
-          <p>Scroll down and let me take you on a tour of my craft!</p>
+        <div className="flex flex-col gap-2 text-gray-100 md:gap-1">
+          <p>
+            Crafting exceptional user experiences from concept to deployment. <br /> I
+            specialize in the Next.js and React ecosystem, building products <br />
+            that users love and teams are proud of.
+          </p>
+          {/* some quick action buttons/links here - view my work - takes to projects, Get in touch - takes to contact, Download Resume - takes to resume */}
+          <div className="mt-10 flex gap-4">
+            <button
+              onClick={() => scrollIntoId("_projects")}
+              className={`flex cursor-pointer items-center justify-center rounded-br-xl rounded-tl-xl border-[0.5px] border-gray-200 px-4 py-2 font-medium text-gray-100 shadow transition-all duration-150 ease-in hover:border-green/10 hover:text-green/70`}
+            >
+              View My Work
+            </button>
+            <button
+              onClick={() => scrollIntoId("_contact-me")}
+              className={`flex cursor-pointer items-center justify-center rounded-br-xl rounded-tl-xl border-[0.5px] border-gray-200 px-4 py-2 font-medium text-gray-100 shadow transition-all duration-150 ease-in hover:border-green/10 hover:text-green/70`}
+            >
+              Get in Touch
+            </button>
+            <a
+              href="/Lekipising-Liplan-Resume.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={`flex cursor-pointer items-center justify-center rounded-br-xl rounded-tl-xl border-[0.5px] border-gray-200 px-4 py-2 font-medium text-gray-100 shadow transition-all duration-150 ease-in hover:border-green/10 hover:text-green/70`}
+            >
+              Download Resume
+            </a>
+          </div>
         </div>
       </div>
       <div className="pic-shadow relative z-50 hidden h-[25vw] w-[25vw] overflow-hidden rounded-full outline outline-green md:block">
@@ -57,7 +87,7 @@ useEffect(() => {
           quality={100}
           priority={true}
           alt="Liplan Lekipising - Portfolio"
-          className="hover:scale-[1.025] object-left bg-left rounded-full transition-all duration-300 ease-in"
+          className="rounded-full bg-left object-left transition-all duration-300 ease-in hover:scale-[1.025]"
           sizes="100vw"
           style={{
             width: "100%",
